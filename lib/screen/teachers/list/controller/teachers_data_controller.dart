71611a6a9a5d7c3f2b25d74dda091feca0928bc6
Sajinduglas <sajinduglas500@gmail.com>
2024-05-24 18:08:17 +0530

    apiadded

--- lib/screen/teachers/list/controller/teachers_data_controller.dart
+++ lib/screen/teachers/list/controller/teachers_data_controller.dart
@@ -1,37 +1,97 @@
+// import 'package:dio/dio.dart';
+// import 'package:get/get.dart';
+//
+// import '../../../../model/teachers_model.dart';
+// import '../../../../utils/contstant/app_config.dart';
+//
+// class TeachersDataController extends GetxController {
+//   final Dio dio = Dio();
+//
+//   RxList<Teachers> getTeachersData() {
+//     return [
+//       Teachers(id: 1, name: "amos", designation: "flutter", salary: 10000),
+//       Teachers(id: 2, name: "najeeb", designation: "flutter", salary: 10000),
+//       Teachers(id: 1, name: "amos", designation: "flutter", salary: 10000),
+//       Teachers(id: 2, name: "najeeb", designation: "flutter", salary: 10000),
+//       Teachers(id: 1, name: "amos", designation: "flutter", salary: 10000),
+//       Teachers(id: 2, name: "najeeb", designation: "flutter", salary: 10000),
+//     ].obs;
+//   }
+//
+//   Future<void> fetchData() async {
+//     final body = {
+//       'query': '''
+// query List(\$filterOptions: ListDpiInput!) {
+//   DpiRate_List(FilterOptions: \$filterOptions) {
+//     list {
+//       _name
+//       _rate
+//       _id
+//     }
+//     totalCount
+//   }
+// }
+// ''',
+//       'variables': {
+//         "filterOptions": {
+//           "branchIds": "6631da5ce9efa0bd84a86852",
+//           "limit": -1,
+//           "skip": 0,
+//           "statusArray": [1]
+//         }
+//       }
+//     };
+//     final String token =
+//         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfdXNlcklkXyI6IjY2MzFkYTVkZTllZmEwYmQ4NGE4NjhmMiIsIl9mb3JjZUxvZ291dF8iOi0xLCJfZGV2aWNlX2lkXyI6IjY2NTA1MjNkNmY4YTQ3MGViMTQ3MTE5OCIsIl91c2VyVHlwZV8iOjAsIl9jcml0aWNhbEVkaXRDb3VudF8iOi0xLCJpYXQiOjE3MTY1Mzk5NjUsImV4cCI6MTcxNzQwMzk2NSwiYXVkIjoiNjYzMWRhNWRlOWVmYTBiZDg0YTg2OGYyIiwiaXNzIjoiTmV4dGVvbnMuY29tIn0.7lP49n4xPrY8DHQ76D-3H9IBlFTj01C6WS-mxgZCGQY";
+//     try {
+//       final response = await dio.post(
+//         AppConfig.url,
+//         data: body,
+//         options: Options(
+//           headers: {
+//             // 'Content-Type': 'application/json',
+//             'X-Tenant-Id': "RL0582",
+//             "Authorization": "Bearer ${token}"
+//           },
+//         ),
+//       );
+//
+//       if(response.statusCode==200){
+//         print("statuscode${response.statusCode}");
+//         print(response.data);
+//       }
+//       else{
+//         print("statuscode${response.statusCode}");
+//       }
+//     } catch (e) {}
+//   }
+// }
+/// chatgpt code
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
-import '../../../../model/teachers_model.dart';
+import '../../../../model/dpirate_model.dart';
 import '../../../../utils/contstant/app_config.dart';
 
 class TeachersDataController extends GetxController {
   final Dio dio = Dio();
 
-  RxList<Teachers> getTeachersData() {
-    return [
-      Teachers(id: 1, name: "amos", designation: "flutter", salary: 10000),
-      Teachers(id: 2, name: "najeeb", designation: "flutter", salary: 10000),
-      Teachers(id: 1, name: "amos", designation: "flutter", salary: 10000),
-      Teachers(id: 2, name: "najeeb", designation: "flutter", salary: 10000),
-      Teachers(id: 1, name: "amos", designation: "flutter", salary: 10000),
-      Teachers(id: 2, name: "najeeb", designation: "flutter", salary: 10000),
-    ].obs;
-  }
+  var dpiRateList = <ListElement>[].obs;
 
   Future<void> fetchData() async {
     final body = {
       'query': '''
-query List(\$filterOptions: ListDpiInput!) {
-  DpiRate_List(FilterOptions: \$filterOptions) {
-    list {
-      _name
-      _rate
-      _id
-    }
-    totalCount
-  }
-}
-''',
+      query List(\$filterOptions: ListDpiInput!) {
+        DpiRate_List(FilterOptions: \$filterOptions) {
+          list {
+            _name
+            _rate
+            _id
+          }
+          totalCount
+        }
+      }
+      ''',
       'variables': {
         "filterOptions": {
           "branchIds": "6631da5ce9efa0bd84a86852",
@@ -41,28 +101,27 @@ query List(\$filterOptions: ListDpiInput!) {
         }
       }
     };
-    final String token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfdXNlcklkXyI6IjY2MzFkYTVkZTllZmEwYmQ4NGE4NjhmMiIsIl9mb3JjZUxvZ291dF8iOi0xLCJfZGV2aWNlX2lkXyI6IjY2NTA1MjNkNmY4YTQ3MGViMTQ3MTE5OCIsIl91c2VyVHlwZV8iOjAsIl9jcml0aWNhbEVkaXRDb3VudF8iOi0xLCJpYXQiOjE3MTY1Mzk5NjUsImV4cCI6MTcxNzQwMzk2NSwiYXVkIjoiNjYzMWRhNWRlOWVmYTBiZDg0YTg2OGYyIiwiaXNzIjoiTmV4dGVvbnMuY29tIn0.7lP49n4xPrY8DHQ76D-3H9IBlFTj01C6WS-mxgZCGQY";
+    final String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfdXNlcklkXyI6IjY2MzFkYTVkZTllZmEwYmQ4NGE4NjhmMiIsIl9mb3JjZUxvZ291dF8iOi0xLCJfZGV2aWNlX2lkXyI6IjY2NTA1MjNkNmY4YTQ3MGViMTQ3MTE5OCIsIl91c2VyVHlwZV8iOjAsIl9jcml0aWNhbEVkaXRDb3VudF8iOi0xLCJpYXQiOjE3MTY1Mzk5NjUsImV4cCI6MTcxNzQwMzk2NSwiYXVkIjoiNjYzMWRhNWRlOWVmYTBiZDg0YTg2OGYyIiwiaXNzIjoiTmV4dGVvbnMuY29tIn0.7lP49n4xPrY8DHQ76D-3H9IBlFTj01C6WS-mxgZCGQY";
+
     try {
       final response = await dio.post(
         AppConfig.url,
         data: body,
         options: Options(
-          headers: {
-            // 'Content-Type': 'application/json',
-            'X-Tenant-Id': "RL0582",
-            "Authorization": "Bearer ${token}"
-          },
+          headers: {'X-Tenant-Id': "RL0582", "Authorization": "Bearer $token"},
         ),
       );
 
-      if(response.statusCode==200){
-        print("statuscode${response.statusCode}");
+      if (response.statusCode == 200) {
+        print(response.statusCode);
         print(response.data);
+        DpiRateModel dpiRateModel = DpiRateModel.fromJson(response.data);
+        dpiRateList.value = dpiRateModel.data?.dpiRateList?.list ?? [];
+      } else {
+        print("Error: ${response.statusCode}");
       }
-      else{
-        print("statuscode${response.statusCode}");
-      }
-    } catch (e) {}
+    } catch (e) {
+      print("Error: $e");
+    }
   }
 }
--- lib/screen/teachers/list/responsive/teachers_data_grid.dart
+++ lib/screen/teachers/list/responsive/teachers_data_grid.dart
@@ -1,10 +1,139 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:nexteons_study/screen/teachers/list/controller/teachers_data_controller.dart';
+// import 'package:nexteons_study/screen/teachers/list/responsive/teachers_data_source.dart';
+// import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+//
+// import '../../../../model/teachers_model.dart';
+//
+// class TeachersDataGrid extends StatefulWidget {
+//   const TeachersDataGrid({super.key});
+//
+//   @override
+//   State<TeachersDataGrid> createState() => _TeachersDataGridState();
+// }
+//
+// class _TeachersDataGridState extends State<TeachersDataGrid> {
+//   final controller=Get.put(TeachersDataController());
+//   late List<Teachers> teachers;
+//
+//   late TeachersDataSource teachersDataSource;
+//
+//   @override
+//   void initState() {
+//     teachers = controller.getTeachersData();
+//     teachersDataSource = TeachersDataSource(teachers);
+//     controller.fetchData();
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: SfDataGrid(
+//             selectionMode: SelectionMode.multiple,
+//             allowSorting: true,
+//             source: teachersDataSource,
+//             columns: [
+//           GridColumn(
+//             columnName: "id",
+//             label: Container(
+//               padding: EdgeInsets.symmetric(horizontal: 16),
+//               alignment: Alignment.centerRight,
+//               child: Text(
+//                 "id",
+//                 overflow: TextOverflow.ellipsis,
+//               ),
+//             ),
+//           ),
+//           GridColumn(
+//             columnName: "name",
+//             label: Container(
+//               padding: EdgeInsets.symmetric(horizontal: 16),
+//               alignment: Alignment.centerRight,
+//               child: Text(
+//                 "name",
+//                 overflow: TextOverflow.ellipsis,
+//               ),
+//             ),
+//           ),
+//           GridColumn(
+//             columnName: "designation",
+//             label: Container(
+//               padding: EdgeInsets.symmetric(horizontal: 16),
+//               alignment: Alignment.centerRight,
+//               child: Text(
+//                 "designation",
+//                 overflow: TextOverflow.ellipsis,
+//               ),
+//             ),
+//           ),
+//           GridColumn(
+//             columnName: "salary",
+//             label: Container(
+//               padding: EdgeInsets.symmetric(horizontal: 16),
+//               alignment: Alignment.centerRight,
+//               child: Text(
+//                 "salary",
+//                 overflow: TextOverflow.ellipsis,
+//               ),
+//             ),
+//           ),
+//         ]));
+//   }
+// }
+//
+//
+//
+// class TeachersDataSource extends DataGridSource {
+//   late List<DataGridRow> dataGridRow;
+//
+//   TeachersDataSource(List<Teachers> teacherss) {
+//     dataGridRow = teacherss
+//         .map<DataGridRow>((dataGridRow) => DataGridRow(cells: [
+//               DataGridCell<int>(columnName: "id", value: dataGridRow.id),
+//               DataGridCell<String>(columnName: "name", value: dataGridRow.name),
+//               DataGridCell(
+//                   columnName: "designation", value: dataGridRow.designation),
+//               DataGridCell(columnName: "salary", value: dataGridRow.salary)
+//             ]))
+//         .toList();
+//   }
+//
+//   List<DataGridRow> get rows => dataGridRow;
+//
+//   @override
+//   DataGridRowAdapter? buildRow(DataGridRow row) {
+//     return DataGridRowAdapter(
+//         cells: row
+//             .getCells()
+//             .map<Widget>((dataGridCell) => Container(
+//                   padding: EdgeInsets.symmetric(horizontal: 16),
+//                   alignment: (dataGridCell.columnName == 'id' ||
+//                           dataGridCell.columnName == 'salary')
+//                       ? Alignment.centerRight
+//                       : Alignment.centerLeft,
+//                   child: Text(
+//                     dataGridCell.value.toString(),
+//                     overflow: TextOverflow.ellipsis,
+//                   ),
+//                 ))
+//             .toList());
+//     throw UnimplementedError();
+//   }
+// }
+//
+//
+///chat gpt
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:nexteons_study/screen/teachers/list/controller/teachers_data_controller.dart';
-import 'package:nexteons_study/screen/teachers/list/responsive/teachers_data_source.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
-import '../../../../model/teachers_model.dart';
+
+import '../../../../model/dpirate_model.dart';
+import '../controller/teachers_data_controller.dart';
+
 
 class TeachersDataGrid extends StatefulWidget {
   const TeachersDataGrid({super.key});
@@ -14,113 +143,97 @@ class TeachersDataGrid extends StatefulWidget {
 }
 
 class _TeachersDataGridState extends State<TeachersDataGrid> {
-  final controller=Get.put(TeachersDataController());
-  late List<Teachers> teachers;
-
-  late TeachersDataSource teachersDataSource;
+  final controller = Get.put(TeachersDataController());
 
   @override
   void initState() {
-    teachers = controller.getTeachersData();
-    teachersDataSource = TeachersDataSource(teachers);
-    controller.fetchData();
     super.initState();
+    controller.fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SfDataGrid(
-            selectionMode: SelectionMode.multiple,
-            allowSorting: true,
-            source: teachersDataSource,
-            columns: [
-          GridColumn(
-            columnName: "id",
-            label: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              alignment: Alignment.centerRight,
-              child: Text(
-                "id",
-                overflow: TextOverflow.ellipsis,
+      body: Obx(() {
+        final teachersDataSource = TeachersDataSource(controller.dpiRateList);
+        return SfDataGrid(
+          selectionMode: SelectionMode.multiple,
+          allowSorting: true,
+          source: teachersDataSource,
+          columns: [
+            GridColumn(
+              columnName: 'id',
+              label: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'ID',
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
             ),
-          ),
-          GridColumn(
-            columnName: "name",
-            label: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              alignment: Alignment.centerRight,
-              child: Text(
-                "name",
-                overflow: TextOverflow.ellipsis,
+            GridColumn(
+              columnName: 'name',
+              label: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Name',
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
             ),
-          ),
-          GridColumn(
-            columnName: "designation",
-            label: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              alignment: Alignment.centerRight,
-              child: Text(
-                "designation",
-                overflow: TextOverflow.ellipsis,
+            GridColumn(
+              columnName: 'rate',
+              label: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'Rate',
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
             ),
-          ),
-          GridColumn(
-            columnName: "salary",
-            label: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              alignment: Alignment.centerRight,
-              child: Text(
-                "salary",
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-          ),
-        ]));
+          ],
+        );
+      }),
+    );
   }
 }
 
 
-
 class TeachersDataSource extends DataGridSource {
-  late List<DataGridRow> dataGridRow;
+  List<DataGridRow> dataGridRows = [];
 
-  TeachersDataSource(List<Teachers> teacherss) {
-    dataGridRow = teacherss
-        .map<DataGridRow>((dataGridRow) => DataGridRow(cells: [
-              DataGridCell<int>(columnName: "id", value: dataGridRow.id),
-              DataGridCell<String>(columnName: "name", value: dataGridRow.name),
-              DataGridCell(
-                  columnName: "designation", value: dataGridRow.designation),
-              DataGridCell(columnName: "salary", value: dataGridRow.salary)
-            ]))
-        .toList();
+  TeachersDataSource(List<ListElement> dpiRateList) {
+    dataGridRows = dpiRateList.map<DataGridRow>((dataGridRow) {
+      return DataGridRow(cells: [
+        DataGridCell<String>(columnName: 'id', value: dataGridRow.id),
+        DataGridCell<String>(columnName: 'name', value: dataGridRow.name),
+        DataGridCell<int>(columnName: 'rate', value: dataGridRow.rate),
+      ]);
+    }).toList();
   }
 
-  List<DataGridRow> get rows => dataGridRow;
+  @override
+  List<DataGridRow> get rows => dataGridRows;
 
   @override
   DataGridRowAdapter? buildRow(DataGridRow row) {
     return DataGridRowAdapter(
-        cells: row
-            .getCells()
-            .map<Widget>((dataGridCell) => Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  alignment: (dataGridCell.columnName == 'id' ||
-                          dataGridCell.columnName == 'salary')
-                      ? Alignment.centerRight
-                      : Alignment.centerLeft,
-                  child: Text(
-                    dataGridCell.value.toString(),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ))
-            .toList());
-    throw UnimplementedError();
+      cells: row.getCells().map<Widget>((dataGridCell) {
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          alignment: dataGridCell.columnName == 'rate' ? Alignment.centerRight : Alignment.centerLeft,
+          child: Text(
+            dataGridCell.value.toString(),
+            overflow: TextOverflow.ellipsis,
+          ),
+        );
+      }).toList(),
+    );
   }
 }
+
 
 
